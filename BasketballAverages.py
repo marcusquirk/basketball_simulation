3710beddd4bed325b46e40594db388218c013cbb
marcus <marcus.quirk@gmail.com>
2020-05-22 19:14:39 -0500

    Removed the class
    BasketballAverages is now simply a file with
    the bespoke methods needed for the program

--- BasketballAverages.py
+++ BasketballAverages.py
@@ -1,19 +1,18 @@
-class BasketballAverages:
+def average(self, values):
+    """finds the mean of a given list of values"""
+    total = 0
+    # Find the sum of all the values
+    for i in range(len(values)):
+        total = total + values[i]
+    # Divide by the number of values
+    mean = total / len(values)
+    return mean
 
-    def average(self, values):
-        """finds the mean of a given list of values"""
-        total = 0
-        # Find the sum of all the values
-        for i in range(len(values)):
-            total = total + values[i]
-        # Divide by the number of values
-        average = total / len(values)
-        return average
 
-    def average_dict(self, values):
-        """finds the mean of a given dictionary of values"""
-        total = 0
-        for i in values:
-            total = total + values[i]
-        average = total / len(values)
-        return average
+def average_dict(self, values):
+    """finds the mean of a given dictionary of values"""
+    total = 0
+    for i in values:
+        total = total + values[i]
+    mean = total / len(values)
+    return mean
