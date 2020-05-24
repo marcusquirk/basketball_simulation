d941540343499de507a96e53b88c5f0930e24990
marcus <marcus.quirk@gmail.com>
2020-05-23 19:03:04 -0500

    Added some test code

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from Database import define_tables
+from Player import add_random_players, read_players
+import Nameset
+
+
 def main():
-    print("Hello world! This is my basketball game.")
+    england_names = Nameset("England_First.json", "England_Last.json")
+
+    startup = input("'new', 'read' or 'add'?")
+    if startup.lower() == 'new':
+        define_tables()
+    elif startup.lower() == 'add':
+        add_random_players(13)
+    elif startup.lower() == 'read':
+        which_player = input("Which player?")
+        read_players(which_player)
+
 
 
 if __name__ == "__main__":
