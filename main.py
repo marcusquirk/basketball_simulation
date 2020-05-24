dc8a04ddf137788c3bb1a11b3876a2b6ecd585df
marcus <marcus.quirk@gmail.com>
2020-05-23 19:47:56 -0500

    Added tests for SQL functionality
    The main method now creates a SQL file with
    5000 randomly-generated players, and some selected
    British basketball teams

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from Database import define_tables
-from Player import add_random_players, read_players
+from Player import add_random_players, read_players, create_player
+from Team import add_teams
 import Nameset
 
 
 def main():
-    england_names = Nameset("England_First.json", "England_Last.json")
-
+    england = Nameset.Nameset("England", "England_First.json", "England_Last.json")
     startup = input("'new', 'read' or 'add'?")
     if startup.lower() == 'new':
         define_tables()
+        add_random_players(5000, england)
+        add_teams()
     elif startup.lower() == 'add':
-        add_random_players(13)
+        add_random_players(13, england)
     elif startup.lower() == 'read':
         which_player = input("Which player?")
         read_players(which_player)
-
 
 
 if __name__ == "__main__":
