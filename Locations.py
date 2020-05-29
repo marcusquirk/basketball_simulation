4879b5a5fb784b760096d11df1895d227eb2d95e
marcus <marcus.quirk@gmail.com>
2020-05-29 09:51:49 -0500

    Built basic locations class

--- Locations.py
+++ Locations.py
@@ -3,57 +3,40 @@ import json
 import random
 import bisect
 
+from Database import create_connection
+from Constants import database
+
 
 class Locations:
 
-    def __init__(self, locations_file):
+    def __init__(self, locations_file, country):
+        self.country = country
+
         json_file = open(os.getcwd() + "/Locations/" + locations_file)
         self.locations = json.load(json_file)
-        print(self.locations)
 
-    """Chooses a name from a given nameset, with a probability (prob) of a double-barrelled name being generated.
-    prob is automatically set to 2.5%.
-    The names in the nameset should be ordered according to a cumulative distribution function (cdf).
-    The get_names function should have already sorted out the cdf."""
-    def gen_forename(self):
+        # Create a cumulative distribution function based on the probability density function.
+        self.locations["cdf"] = self.locations["pdf"]
+        for i in range(1, len(self.locations["pdf"])):
+            self.locations["cdf"][i] = self.locations["cdf"][i-1]+self.locations["cdf"][i]
+
+    def gen_location(self):
         # Generate a random number between 0 and the total cumulative frequency of names
-        randnum = random.random() * self.forenames["cdf"][-1]
+        randnum = random.random() * self.locations["cdf"][-1]
         # Choose a name
-        name_num = bisect.bisect(self.forenames["cdf"], randnum)
+        name_num = bisect.bisect(self.locations["cdf"], randnum)
+        return self.locations["city"][name_num]
 
-        # Small chance of a double-barrelled name
-        compound = random.random()
-        if compound < self.forename_prob:
-            name_num2 = name_num
-            # Makes sure the two names aren't the same
-            while name_num2 == name_num:
-                randnum2 = random.random() * self.forenames["cdf"][-1]
-                name_num2 = bisect.bisect(self.forenames["cdf"], randnum2)
-            # Return the double-barrelled name with a hyphen in between
-            return self.forenames["name"][name_num] + "-" + self.forenames["name"][name_num2]
-        # If no double-barrelled name is generated, return the single name
-        else:
-            return self.forenames["name"][name_num]
-
-    def gen_surname(self):
-        # Generate a random number between 0 and the total cumulative frequency of names
-        randnum = random.random() * self.surnames["cdf"][-1]
-        # Choose a name
-        name_num = bisect.bisect(self.surnames["cdf"], randnum)
-
-        # Small chance of a double-barrelled name
-        compound = random.random()
-        if compound < self.surname_prob:
-            name_num2 = name_num
-            # Makes sure the two names aren't the same
-            while name_num2 == name_num:
-                randnum2 = random.random() * self.surnames["cdf"][-1]
-                name_num2 = bisect.bisect(self.surnames["cdf"], randnum2)
-            # Return the double-barrelled name with a hyphen in between
-            return self.surnames["name"][name_num] + "-" + self.surnames["name"][name_num2]
-        # If no double-barrelled name is generated, return the single name
-        else:
-            return self.surnames["name"][name_num]
+    def to_database(self):
+        conn = create_connection(database)
+        with conn:
+            cur = conn.cursor()
+            current_id = cur.lastrowid
+            for i in range(len(self.locations["pdf"])):
+                data = [current_id, self.locations["city"][i], self.country, self.locations["pdf"][i]]
+                sql = """INSERT INTO locations (id, place, country, cdf)
+                        VALUES(?, ?, ?, ?)"""
+                cur.execute(sql, data)
 
     def get_country(self):
         return self.country
