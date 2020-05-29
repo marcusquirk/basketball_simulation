9a70a325ceaf32d8ebe92c82fc6038cd1adf53b5
marcus <marcus.quirk@gmail.com>
2020-05-29 09:52:01 -0500

    Added testing for locations class

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ import Locations
 
 
 def main():
-    england = Nameset.Nameset("England", "England_First.json", "England_Last.json")
+    locations = {"england": Locations.Locations("England.json", "England")}
+    names = {"england": Nameset.Nameset("England", "England_First.json", "England_Last.json")}
     startup = input("'new', 'read' or 'add'?")
     if startup.lower() == 'new':
         define_tables()
-        add_random_players(5000, england)
+        add_random_players(5000, names["england"])
         add_teams()
     elif startup.lower() == 'add':
-        add_random_players(13, england)
+        add_random_players(13, names["england"])
     elif startup.lower() == 'read':
         which_player = input("Which player?")
         read_players(which_player)
-    locations = Locations.Locations("Locations.json")
-    print(locations.locations)
+    locations["england"].to_database()
+    print(locations["england"])
 
 
 if __name__ == "__main__":
