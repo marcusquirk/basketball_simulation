8e302a13ce8c1c45568cffdd19f5c0fcbf7ebbab
marcus <marcus.quirk@gmail.com>
2020-06-02 05:21:47 -0500

    Test hometowns

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ import Locations
 
 
 def main():
-    locations = {"england": Locations.Locations("England.json", "England")}
-    names = {"england": Nameset.Nameset("England", "England_First.json", "England_Last.json")}
+    england = {"names": Nameset.Nameset("England", "England_First.json", "England_Last.json"),
+               "locations": Locations.Locations("England.json", "England")}
+
     startup = input("'new', 'read' or 'add'?")
     if startup.lower() == 'new':
         define_tables()
-        add_random_players(5000, names["england"])
+        add_random_players(5000, england)
         add_teams()
     elif startup.lower() == 'add':
-        add_random_players(13, names["england"])
+        add_random_players(13, england)
     elif startup.lower() == 'read':
         which_player = input("Which player?")
         read_players(which_player)
-    locations["england"].to_database()
-    print(locations["england"])
+    england["locations"].to_database()
 
 
 if __name__ == "__main__":
