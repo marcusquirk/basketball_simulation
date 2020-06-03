27d9c602119b19bc7945704d61b24e69b0cb7f27
marcus <marcus.quirk@gmail.com>
2020-06-03 07:38:30 -0500

    Updated to test multiple home countries

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Database import define_tables
+import Player
 from Player import add_random_players, read_players, create_player
 from Team import add_teams
 import Nameset
@@ -7,19 +8,23 @@ import Cities
 
 def main():
     england = {"names": Nameset.Nameset("England", "England_First.json", "England_Last.json"),
-               "locations": Cities.Cities("England.json", "England")}
+               "cities": Cities.Cities("England.json", "England")}
+    scotland = {"names": Nameset.Nameset("Scotland", "Scotland_First.json", "Scotland_Last.json"),
+                "cities": Cities.Cities("Scotland.json", "Scotland")}
 
     startup = input("'new', 'read' or 'add'?")
     if startup.lower() == 'new':
         define_tables()
-        add_random_players(5000, england)
+        add_random_players(250, england)
+        add_random_players(50, scotland)
         add_teams()
     elif startup.lower() == 'add':
         add_random_players(13, england)
     elif startup.lower() == 'read':
         which_player = input("Which player?")
         read_players(which_player)
-    england["locations"].to_database()
+    england["cities"].to_database()
+    scotland["cities"].to_database()
 
 
 if __name__ == "__main__":
